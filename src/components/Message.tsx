4e70cfd27b2abf1db04b45a333dc652968d87b40
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-09-02 23:53:19 +0300

    Different colors for messages

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,15 +3,16 @@ interface Message {
   username: string;
   date: string;
   children: string;
+  userColor: string;
 }
 
-const Message = ({ userImg, username, date, children }: Message) => {
+const Message = ({ userImg, username, date, children, userColor }: Message) => {
   return (
     <div className="message">
       <img className="userImg" src={userImg} alt="" />
       <div className="content">
         <div className="message-info">
-          <h4 className="username">{username}</h4>
+          <h4 className={`username ${userColor}`}>{username}</h4>
           <p className="date">{date}</p>
         </div>
         <p className="message-content">{children}</p>
